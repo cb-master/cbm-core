7462ef9a48c1992ba139e2837cae95b912e848da
Cloud Bill Master <strblackhawk@gmail.com>
2024-12-07 01:03:11 +0600

    option migrate and vault added

---
+++ src/Option.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * APP Name:        Laika Framework Core
+ * APP Provider:    Showket Ahmed
+ * APP Link:        https://cloudbillmaster.com
+ * APP Contact:     riyadtayf@gmail.com
+ * APP Version:     1.0.0
+ * APP Company:     Cloud Bill Master Ltd.
+ */
+
+// Namespace
+namespace CBM\Core;
+
+use CBM\Model\Model;
+
+class Option
+{
+    // Get Option Value
+    public static function get(string $name)
+    {
+        $option = Model::table('options')->select()->filter('option_key', '=', $name)->single();
+        return $option->option_value ?? $option['option_value'] ?? '';
+    }
+
+    // Set Option
+    public static function set(string $name, string $value):int
+    {
+        return Model::table('options')->replace(['option_key' => $name, 'option_value' => $value]);
+    }
+}
---
+++ src/Vault/Vault.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * APP Name:        Laika Framework Core
+ * APP Provider:    Showket Ahmed
+ * APP Link:        https://cloudbillmaster.com
+ * APP Contact:     riyadtayf@gmail.com
+ * APP Version:     1.0.0
+ * APP Company:     Cloud Bill Master Ltd.
+ */
+
+// Namespace
+namespace CBM\Core\Vault;
+
+use CBM\Core\Option;
+
+class Vault
+{
+    // Get a Random Key
+    /**
+     * @param int $byte - Default Value is 16.
+     */
+    public static function randomKey(int $byte = 16):string
+    {
+        return bin2hex(random_bytes($byte));
+    }
+
+    // Get Open SSL Cipher Passphrase
+    private static function secret():string
+    {
+        $appsecret = Option::get('appsecret');
+        if(!$appsecret){
+            $appsecret = self::randomKey();
+            Option::set('appsecret', $appsecret);
+        }
+        return $appsecret;
+    }
+
+    // Get Open SSL Cipher IV
+    private static function iv():string
+    {
+        $appiv = Option::get('appiv');
+        if(!$appiv){
+            $appiv = openssl_random_pseudo_bytes(openssl_cipher_iv_length(ENCRYPTION_METHOD));
+            Option::set('appiv', $appiv);
+        }
+        return $appiv;
+    }
+
+    // Encrypt String
+    /**
+     * @param string $string - Required Argument.
+     */
+    public static function encrtpt(string $string):string
+    {
+        return openssl_encrypt($string, ENCRYPTION_METHOD, self::secret(), 0, self::iv());
+    }
+
+    // Decrypt Encrypted String
+    /**
+     * @param string $string - Required Argument.
+     */
+    public static function decrypt(string $string):string
+    {
+        return openssl_decrypt($string, ENCRYPTION_METHOD, self::secret(), 0, self::iv());
+    }
+}
