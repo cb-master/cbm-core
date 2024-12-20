bae42bd156248d6b635b9b3a8fba1125e472f436
Cloud Bill Master <strblackhawk@gmail.com>
2024-12-21 00:21:47 +0600

    action and filter modified

--- src/Filter.php
+++ src/Filter.php
@@ -14,12 +14,15 @@ class Filter
     // Filters
     private static $filters = [];
 
+    // Actions
+    private static $actions = [];
+
     // Add Filter
     /**
      * @param string $filter - Required Argument as filter name.
      * @param callable $callback - Required Argument as function.
      */
-    public static function add(string $filter, callable $callback)
+    public static function add_filter(string $filter, callable $callback)
     {
         self::$filters[$filter][] = $callback;
     }
@@ -29,13 +32,13 @@ class Filter
      * @param string $filter - Required Argument as filter name.
      * @param mixed ...$args - Required Argument as function parameters.
      */
-    public static function do(string $filter, mixed ...$args)
+    public static function do_filter(string $filter, mixed ...$args):mixed
     {
         // Output
         $output = [];
 
         if(!isset(self::$filters[$filter])){
-            throw new Error("'{$filter}' Filter Not Found!");
+            throw new Error("'{$filter}' Filter Does Not Exist!");
         }
         
         foreach(self::$filters[$filter] as $callback){
@@ -43,5 +46,31 @@ class Filter
         }
 
         return $output;
+    }
+
+    // Add Action
+    /**
+     * @param string $action - Required Argument as action name.
+     * @param callable $callback - Required Argument as function.
+     */
+    public static function add_action(string $action, callable $callback):void
+    {
+        if(isset(self::$actions[$action])){
+            throw new Error("'{$action}' Action Already Exists!");
+        }
+        self::$actions[$action] = $callback;
+    }
+
+    // Do Action
+    /**
+     * @param string $action - Required Argument as action name.
+     * @param mixed ...$args - Required Argument as function parameters.
+     */
+    public static function do_action(string $action, mixed ...$args):mixed
+    {
+        if(!isset(self::$actions[$action])){
+            throw new Error("'{$action}' Filter Does Not Exist!");
+        }        
+        return call_user_func(self::$actions[$action], ...$args);
     }
 }
