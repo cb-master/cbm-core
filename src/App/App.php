3631167592bdd4919d5c8c8378f479af852525b5
Cloud Bill Master <strblackhawk@gmail.com>
2025-01-20 15:58:48 +0600

    Core App Controller Updated

--- src/App/App.php
+++ src/App/App.php
@@ -35,15 +35,16 @@ class App
 
         // Get Class & Method
         $method = Uri::slug(1) ?: 'index';
-        $class = (class_exists($controller) && method_exists($controller, $method)) ? $controller : $_404;
+        $acceptedMethods = get_class_methods($controller);
+        $class = (class_exists($controller) && in_array($method, $acceptedMethods)) ? $controller : $_404;
 
         ($class == $_404) ? Response::set(404) : Response::set(200);
 
-        // Check Method Exist
-        $method = method_exists($controller, $method) ? $method : 'index';
-
         // Get Controller Class Object
         $object = new $class;
+
+        // Check Method Exist
+        $method = method_exists($controller, $method) && in_array($method, $acceptedMethods) ? $method : 'index';
 
         // Load Controller & Method
         call_user_func([$object, $method]);
