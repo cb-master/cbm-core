a3f0ed8adf72ae2d96abd53e839c4ea6abdf61a4
Cloud Bill Master <strblackhawk@gmail.com>
2025-02-04 01:38:14 +0600

    Reuest Updated

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -5,207 +5,165 @@
  * Author Email: riyadhtayf@gmail.com
  */
 
-// Forbidden Access
-defined('ROOTPATH') || http_response_code(403).die('403 Forbidden Access!');
+// Namespace
+namespace CBM\Core\Request;
 
-use CBM\Core\Request\Request;
-use CBM\Core\Helper\Helper;
-use CBM\Core\Config\Config;
-use CBM\Session\Session;
+use CBM\Core\Uri\Uri;
+use CBM\CoreHelper\Resource;
 
-///////////////////////////////////////
-////////// COMMON FUNCTIONS ///////////
-///////////////////////////////////////
-
-// Get Visitor IP
-function getIP():string
+class Request Extends Resource
 {
-    return Helper::get_client_ip();
-}
+    // Instance
+    private static Object|Null $instance = Null;
 
-// Key Value From Object or Array
-function keyValue(object|array $data, string $key):mixed
-{
-    return $data->{$key} ?? $data[$key] ?? '';
-}
+    // Method
+    private string $method;
 
-// Request is Post
-function isPost():bool
-{
-    return Request::isPost();
-}
-
-// Get $_POST Request Data
-function posted():array
-{
-    return Request::posted();
-}
-
-// Get Post Key Data
-function post(string $key):mixed
-{
-    return Request::isPost() ? ($key && array_key_exists($key, Request::data()) ? Request::data()[$key] : '') : '';
-}
-
-// Get $_GET Request Data
-function inputs():array
-{
-    return Request::inputs();
-}
-
-// Get Get Request Data
-function get(string $key):mixed
-{
-    return Request::isGet() ? ($key && array_key_exists($key, Request::data()) ? Request::data()[$key] : '') : '';
-}
-
-// Request is Get
-function isGet():bool
-{
-    return Request::isGet();
-}
-
-// Request Key Value
-function requestKey(string $key):string|array
-{
-    return Request::key($key);
-}
-
-// Request Datas
-function requestData():array
-{
-    return Request::data();
-}
-
-// Add Hook
-function register_hook(string $hook, callable $callback):void
-{
-    Hooks::register($hook, $callback);
-}
-
-// Do Hook
-function trigger_hook(string $hook, mixed ...$args):mixed
-{
-    return Hooks::trigger($hook, ...$args);
-}
-
-//////////////////////////////////////
-////////// STAFF FUNCTIONS ///////////
-//////////////////////////////////////
-
-// Get Staff Token
-function staffToken():string
-{
-    return Session::get('token', ADMIN);
-}
-
-// Get Staff Id
-function staffId():string
-{
-    return Session::get('id', ADMIN);
-}
-
-// Get Staff First Name
-function staffFirstName():string
-{
-    return Session::get('fname', ADMIN);
-}
-
-// Get Staff Last Name
-function staffLastName():string
-{
-    return Session::get('lname', ADMIN);
-}
-
-// Get Staff Full Name
-function staffName():string
-{
-    return Session::get('name', ADMIN);
-}
-
-// Get Staff Username
-function staffUsername():string
-{
-    return Session::get('username', ADMIN);
-}
-
-// Get Staff Uuid
-function staffUuid():string
-{
-    return Session::get('uuid', ADMIN);
-}
-
-// Get Staff Email
-function staffEmail():string
-{
-    return Session::get('email', ADMIN);
-}
-
-/////////////////////////////////////////
-////////// DATABASE FUNCTIONS ///////////
-/////////////////////////////////////////
-
-// Get Page Number
-function limit():int
-{
-    return Config::get('app', 'limit');
-}
-
-// Get Page Number
-function offset():int
-{
-    return (pageNumber() - 1) * limit();
-}
-
-//////////////////////////
-////////// API ///////////
-//////////////////////////
-
-function fetch(string $slug, string $method = 'get', array|object $data = [])
-{
-    $method = strtoupper($method);
-    $slug = trim($slug);
-    $isLink = parse_url($slug, PHP_URL_HOST);
-    $link = $isLink ? $slug : WEBHOST."/api/{$slug}";
-    $response = [
-        'code'  =>  'AP_CURL_ERROR',
-        'status'=>  'failed',
-        'data'  =>  []
-    ];
-    // Curl Options
-    echo $link;
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $link);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_MAXREDIRS, 10);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
-    if($method != 'GET'){
-        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data), JSON_FORCE_OBJECT);
+    // Initiate Request Class
+    public function __construct()
+    {
+        $this->method = strtolower($_SERVER['REQUEST_METHOD']);
     }
-    curl_setopt($ch, CURLOPT_HTTPHEADER, [
-        "Authorization:".staffToken(),
-        "Content-Type:application/json"
-    ]);
 
-    try{
-        $res = curl_exec($ch);
-        $response = json_decode($res);
-    }catch(\Throwable $th) {
-        $response['message'] = curl_error($ch);
+    // Load Instance
+    public static function instance()
+    {
+        return self::$instance ?: new Static;
     }
-    curl_close($ch);
-    return $response;
-}
 
-/////////////////////////////////////
-////////// EMAIL FUNCTION ///////////
-/////////////////////////////////////
+    // Get Method
+    public static function method():string
+    {       
+        // Return Value
+        return self::instance()->method;
+    }
 
-// Send Email
-function send_email(string $message, string $from, string $to, ?string $reply = null):bool
-{
-    return true;
+    // Method is Post
+    public static function isPost():bool
+    {
+        // Return Value
+        return self::method() === 'post';
+    }
+
+    // Method is Get
+    public static function isGet():bool
+    {
+        // Return Value
+        return self::method() === 'get';
+    }
+
+    // Requested Data
+    /**
+     * @param array $data - Default Value is []
+     */
+    public static function data():array
+    {
+        // Return Request Data
+        return self::instance()->purify($_REQUEST);
+    }
+
+    // Request Key Value
+    /**
+     * @param string $key - Required Argument
+     */
+    public static function key(string $key):string|array
+    {
+        // Return Data
+        return self::instance()->data()[$key] ?? '';
+    }
+    
+    // Get Post Data
+    /**
+     * @param string $key - Required Argument
+     */
+    public static function post(string $key):string|array
+    {
+        // Return Data
+        return self::posted()[$key] ?? '';
+    }
+
+    // Get $_POST Request Data
+    public static function posted():array
+    {
+        return self::instance()->purify($_POST);
+    }
+
+    // Get Get Data
+    /**
+     * @param string $key - Required Argument
+     */
+    public static function get(string $key):string
+    {
+        // Return Data
+        return self::inputs()[$key] ?? '';
+    }
+
+    // Get $_GET Request Data
+    public static function inputs():array
+    {
+        return self::instance()->purify($_GET);
+    }
+
+    // Get Requested Files
+    public static function files()
+    {
+        // Return Data
+        return $_FILES;
+    }
+
+    // Request Files
+    public function file(string $key):array
+    {
+        // Return Data
+        return $_FILES[$key] ?? [];
+    }
+
+    // Validate Request Keys
+    /**
+     * @param string|array $keys - Required Argument
+     * @param string $location - Required Argument. Example 'sample' or 'sample/url'
+     */
+	public static function validate(string|array $keys, string $location)
+	{
+        $keys = (is_string($keys)) ? [$keys] : $keys;
+
+        $location = trim($location, "/");
+
+        $redirect = Uri::app_uri()."/{$location}/";
+        
+		$errors = [];
+		foreach($keys as $key):
+			if(!array_key_exists($key, self::data()))
+			{
+				$errors[] = sprintf("Request Key '<b style='color:red'>%s</b>' Missing.", $key);
+			}
+		endforeach;
+
+		if($errors)
+		{
+            self::request_validator_message($errors, $redirect);
+		}
+	}
+
+    // Request Data Purify
+    /**
+     * @param array $keys - Default Value is []
+     */
+    public function purify(array $data = [])
+    {
+        $request_data = [];
+        // Clear Request Data
+        $data = $data ?: $_REQUEST;
+        foreach($data as $key => $value){
+            $request_data[$key] = is_array($value) ? $this->purify($value) : htmlspecialchars(trim($value));
+        }
+        // Return Request Data
+        return $request_data;
+    }
+
+    // Check If Request For Api
+    public static function apiOnly():bool
+    {
+        return $_SERVER['REMOTE_ADDR'] === $_SERVER['SERVER_ADDR'];
+    }
 }
